1050aad055cdc3d09b08469982bc33c49665396a
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-04-05 04:15:03 +0600

    Home section design done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,1 +1,22 @@
 
+import React from "react";
+import './Home.css'
+import watch from "../../Images/Watch.png";
+
+const Home = () => {
+  return (
+    <div className="container d-flex watch-container mx-auto">
+      <div className="watch-description col-md-6 col-sm-12">
+        <p>SMART WATCH</p>
+        <h1 className="watch-title">This will change <br/> your life forever!</h1>
+        <p>A smartwatch is a portable device that's designed to be worn on a wrist. Like smartphones, they use touchscreens.</p>
+        <button className="live-btn">Live Demo</button>
+      </div>
+      <div className="col-md-6 col-sm-12">
+        <img className="img-fluid" src={watch} alt="" srcset="" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
