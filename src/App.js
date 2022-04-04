88b3ce11b3eb019ecf9fea1e2de21fabde48f666
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-04-04 15:20:13 +0600

    first commit , react dom and all components creat and router set complite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/reviews'element={<Reviews/>}/>
+        <Route path='/dashboard'element={<DashBoard/>}/>
+        <Route path='/blogs'element={<Blogs/>}/>
+        <Route path='/about'element={<About/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1>This is blogs page
+                
+            </h1>
+        </div>
+    );
+};
+
+export default Blogs;
